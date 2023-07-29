e7971d847617ddad17d0cc9d90940a23a0a7daf4
huynq55 <huynq23091@gmail.com>
2023-07-29 15:03:50 +0700

    Update streamlit_openai_pinecone.py

--- streamlit_openai_pinecone.py
+++ streamlit_openai_pinecone.py
@@ -5,7 +5,7 @@ import pinecone
 # Pinecone and OpenAI setup
 pinecone_api_key = "23c095af-b6e1-453a-98e4-b09a2804ba46"
 pinecone_env = "us-central1-gcp"
-openai_api_key = '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'
+openai_api_key = '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'
 openai.api_key = openai_api_key
 
 st.title('OpenAI-Pinecone Search App')
