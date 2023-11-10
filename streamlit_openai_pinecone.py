fe95abeeb9ed5ccf376c0a78000a7d46fc7b7938
huynq55 <huynq23091@gmail.com>
2023-11-10 17:54:34 +0700

    Update streamlit_openai_pinecone.py

--- streamlit_openai_pinecone.py
+++ streamlit_openai_pinecone.py
@@ -52,9 +52,9 @@ if submit_button and search_query:
         #text = match['metadata']['text']
         #text = match['text']
         print(match)
-        highlighted_text = re.sub(r'(\b' + r'\b|\b'.join(query_words) + r'\b)', r'**\1**', text, flags=re.IGNORECASE)
+        #highlighted_text = re.sub(r'(\b' + r'\b|\b'.join(query_words) + r'\b)', r'**\1**', text, flags=re.IGNORECASE)
 
-        st.write(f"{match['score']:.2f}: {sutta_url}")
-        st.markdown(highlighted_text)
+        #st.write(f"{match['score']:.2f}: {sutta_url}")
+        #st.markdown(highlighted_text)
 elif submit_button and not search_query:
     st.write('Please enter a search query')
